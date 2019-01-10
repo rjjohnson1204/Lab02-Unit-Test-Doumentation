949eb236f9f7fe08cddb71606d6f820315458cb8
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-09 22:22:46 -0800

    created balance and spaced buttons

--- unit_test/unit_test/Program.cs
+++ unit_test/unit_test/Program.cs
@@ -4,21 +4,29 @@ namespace unit_test
 {
     class Program
     {
+        public static decimal MyBalance = 500.00m;
+
         static void Main(string[] args)
         {
             //intro to atm//
-            Console.WriteLine("-----------------------");
-            Console.WriteLine(" Welcome to Shady ATM");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("Enter Social Sec...I mean...PIN");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("      Make a Selection       ");
-            Console.WriteLine("1. Check Balance");
-            Console.WriteLine("2. Make a Withdrawl");
-            Console.WriteLine("3. Make a Deposit");
-            Console.WriteLine("4. Exit ");
+            Console.WriteLine("                                     ");
+            Console.WriteLine("                $$ Welcome to Shady ATM $$");
+            Console.WriteLine("                                     ");
+            Console.WriteLine("                                     ");
+            Console.WriteLine("           $$ Enter Social Sec...I mean...PIN $$");
+            Console.WriteLine("                                     ");
+            Console.WriteLine("                 -----------------------");
+            Console.WriteLine("                                     ");
+            // ask user to make a selection//
+            Console.WriteLine("                                     ");
+            Console.WriteLine("                    Make a Selection       ");
+            Console.WriteLine("                                     ");
+
+            Console.WriteLine("                   1. Check Balance");
+            Console.WriteLine("                   2. Make a Withdrawl");
+            Console.WriteLine("                   3. Make a Deposit");
+            Console.WriteLine("                   4. Doation to Nigerian Prince in to flee presecution  ");
+            Console.WriteLine("                   5. Exit ");
 
 
 
