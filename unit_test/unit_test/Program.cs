1b5b3a39b8bd26e734525c41cc0239442f2d3bbe
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-09 21:28:07 -0800

    atm buttons

--- unit_test/unit_test/Program.cs
+++ unit_test/unit_test/Program.cs
@@ -6,8 +6,26 @@ namespace unit_test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //intro to atm//
+            Console.WriteLine("-----------------------");
+            Console.WriteLine(" Welcome to Shady ATM");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Enter Social Sec...I mean...PIN");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("      Make a Selection       ");
+            Console.WriteLine("1. Check Balance");
+            Console.WriteLine("2. Make a Withdrawl");
+            Console.WriteLine("3. Make a Deposit");
+            Console.WriteLine("4. Exit ");
+
+
+
+
         }
+
+
     }
 }
 
