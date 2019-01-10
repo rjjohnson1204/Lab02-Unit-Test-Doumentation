0b0b1ccfce35cfac5672bcb359a3acd1498054fe
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-09 23:33:34 -0800

    switch, updated readme, and working on output for input 4

--- unit_test/unit_test/Program.cs
+++ unit_test/unit_test/Program.cs
@@ -4,9 +4,16 @@ namespace unit_test
 {
     class Program
     {
+        // "public" removes restrictios on accessing balance//
         public static decimal MyBalance = 500.00m;
 
-        static void Main(string[] args)
+        // starts/ calls ATM //
+        public static void Main(string[] args)
+        {
+            ATM();  
+        }
+
+        static void ATM()
         {
             //intro to atm//
             Console.WriteLine("                                     ");
@@ -21,18 +28,112 @@ namespace unit_test
             Console.WriteLine("                                     ");
             Console.WriteLine("                    Make a Selection       ");
             Console.WriteLine("                                     ");
-
             Console.WriteLine("                   1. Check Balance");
             Console.WriteLine("                   2. Make a Withdrawl");
             Console.WriteLine("                   3. Make a Deposit");
-            Console.WriteLine("                   4. Doation to Nigerian Prince in to flee presecution  ");
+            Console.WriteLine("                   4. Donation to Nigerian Prince in to flee presecution  ");
             Console.WriteLine("                   5. Exit ");
-
-
-
-
         }
+        public static void MenuSelection(int selection)
+        {
+            //switch is a selection statement that chooses a single switch section to execute from a list of candidates based on a pattern match with the match expression.
+            switch (selection)
+            {
+                case 1:
+                    //Display balance
 
+                    //CheckBalance(MyBalance);
+                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                    Console.ReadKey();
+                    // Clears the console buffer and corresponding console window of display information.
+                    Console.Clear();
+                    break;
+
+                case 2:
+                    //Make withdrawl from MyBalance
+
+                    //Withdrawl(MyBalance);
+                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                    Console.ReadKey();
+                    // Clears the console buffer and corresponding console window of display information.
+                    Console.Clear();
+                    break;
+
+                case 3:
+                    //Make Deposit into MyBalance
+
+                    //Deposit(MyBalance);
+                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                    Console.ReadKey();
+                    // Clears the console buffer and corresponding console window of display information.
+                    Console.Clear();
+                    break;
+
+                case 4:
+                    //Loss of retirement fund. Identity Stolen. BANKRUPCY!
+
+                    Donation();
+                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                    Console.ReadKey();
+                    // Clears the console buffer and corresponding console window of display information.
+                    Console.Clear();
+                    break;
+
+                case 5:
+                    //Exit of ATM
+
+                    //Exit();
+                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                    Console.ReadKey();
+                    // Clears the console buffer and corresponding console window of display information.
+                    Console.Clear();
+                    break;
+
+            }
+        }
+        public static void Donation()
+        {
+            Console.ReadLine();
+
+            Console.WriteLine($@"
+
+
+        Loss of retirement fund. Identity Stolen.
+´´´´´´´´´´´´´´´´´´´ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´`
+´´´´´´´´´´´´´´´´´¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´
+´´´´´´´´´´´´´¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´´
+´´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´
+´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´`´´´´´´´´´´´¶¶´´´´´´´´´´`
+´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´
+´´´´´´´´´´´¶¶´¶¶´´´¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´¶¶´¶¶´´´´´´´´´´´
+´´´´´´´´´´´´¶¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶¶¶´´´´´´´´´´´
+´´´´´´´´´´´´´¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶´´´´´´´´´´´´´
+´´´´¶¶¶´´´´´´´¶¶´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´¶¶´´´´´´¶¶¶¶´´´
+´´´¶¶¶¶¶´´´´´¶¶´´´¶¶¶¶¶¶¶´´´¶¶¶´´´¶¶¶¶¶¶¶´´´¶¶´´´´´¶¶¶¶¶¶´´
+´´¶¶´´´¶¶´´´´¶¶´´´´´¶¶¶´´´´¶¶¶¶¶´´´´¶¶¶´´´´´¶¶´´´´¶¶´´´¶¶´´
+´¶¶¶´´´´¶¶¶¶´´¶¶´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´¶¶¶¶´´´´¶¶¶´
+¶¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´¶¶
+¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶¶´´´´´´¶¶¶¶¶¶¶¶
+´´¶¶¶¶´¶¶¶¶¶´´´´´´¶¶¶¶¶´´´´´´´´´´´´´´¶¶¶´¶¶´´´´´¶¶¶¶¶¶´¶¶¶´
+´´´´´´´´´´¶¶¶¶¶¶´´¶¶¶´´¶¶´´´´´´´´´´´¶¶´´¶¶¶´´¶¶¶¶¶¶´´´´´´´´
+´´´´´´´´´´´´´´¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´´´´´¶¶´¶¶´¶´¶´¶´¶´¶´¶´¶´¶´¶¶´´´´´´´´´´´´´´´´´
+´´´´´´´´´´´´´´´´¶¶¶¶´´¶´¶´¶´¶´¶´¶´¶´¶´´´¶¶¶¶¶´´´´´´´´´´´´´´
+´´´´´´´´´´´´¶¶¶¶¶´¶¶´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´´¶¶´¶¶¶¶¶´´´´´´´´´´´´
+´´´´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶´´´´´´´´´´´´´´´´´¶¶´´´´´´¶¶¶¶¶¶¶¶¶´´´´
+´´´¶¶´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´´
+´´´´¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶´´´´
+´´´´´´¶¶´´´¶¶¶´´´´´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´´´´¶¶¶´´´¶¶´´´´´´
+´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´
+´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´ 
+                ");
+            Console.ReadLine();
+        }
 
     }
 }
