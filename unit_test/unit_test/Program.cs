d526a1e861d7a46e8137e388f56ba6a36f90b85d
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-01-19 13:18:33 -0800

    got withdraw working

--- unit_test/unit_test/Program.cs
+++ unit_test/unit_test/Program.cs
@@ -4,161 +4,185 @@ namespace unit_test
 {
     class Program
     {
-        // "public" removes restrictios on accessing balance//
-        public static decimal MyBalance = 500.00m;
-
-        // starts/ calls ATM //
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            ATM();  
-        }
 
-        static void ATM()
-        {
-            //intro to atm//
-            Console.WriteLine("                                     ");
-            Console.WriteLine("                $$ Welcome to Shady ATM $$");
-            Console.WriteLine("                                     ");
-            Console.WriteLine("                                     ");
-            Console.WriteLine("           $$ Enter Social Sec...I mean...PIN $$");
-            Console.WriteLine("                                     ");
-            Console.WriteLine("                 -----------------------");
-            Console.WriteLine("                                     ");
-            // ask user to make a selection//
-            Console.WriteLine("                                     ");
-            Console.WriteLine("                    Make a Selection       ");
-            Console.WriteLine("                                     ");
-            Console.WriteLine("                   1. Check Balance");
-            Console.WriteLine("                   2. Make a Withdrawl");
-            Console.WriteLine("                   3. Make a Deposit");
-            Console.WriteLine("                   4. Donation to Nigerian Prince flee from presecution  ");
-            Console.WriteLine("                   5. Exit ");
+             int MyBalance = 5000;
 
-            Console.WriteLine("                   Please choose 1, 2, 3, 4, or 5             ");
-            Console.WriteLine("                                     ");
-
-
-            try
-            {
-                int selection = Convert.ToInt32(Console.ReadLine());
-                MenuSelection(selection);
-            }
-
-            catch (Exception)
-            {
-                Console.Write("Improper selection. Make another selection");
-                Console.Clear();
-            }
-           
-
-        }
-        public static void MenuSelection(int selection)
-        {
+            ATM();
             
-            //switch is a selection statement that chooses a single switch section to execute from a list of candidates based on a pattern match with the match expression.
-            switch (selection)
+            void ATM()
             {
-                case 1:
-                    //Display balance
+                //intro to atm//
+                Console.WriteLine("                                     ");
+                Console.WriteLine("                $$ Welcome to Shady ATM $$");
+                Console.WriteLine("                                     ");
+                Console.WriteLine("                                     ");
+                Console.WriteLine("           $$ Enter Social Sec...I mean...PIN $$");
+                Console.WriteLine("                                     ");
+                Console.WriteLine("                 -----------------------");
+                Console.WriteLine("                                     ");
+                // ask user to make a selection//
+                Console.WriteLine("                                     ");
+                Console.WriteLine("                    Make a Selection       ");
+                Console.WriteLine("                                     ");
+                Console.WriteLine("                   1. Check Balance");
+                Console.WriteLine("                   2. Make a Withdrawl");
+                Console.WriteLine("                   3. Make a Deposit");
+                Console.WriteLine("                   4. Donation to Nigerian Prince flee from presecution  ");
+                Console.WriteLine("                   5. Exit ");
+
+                Console.WriteLine("                   Please choose 1, 2, 3, 4, or 5             ");
+                Console.WriteLine("                                     ");
+
+
+                try
+                {
+                    int selection = Convert.ToInt32(Console.ReadLine());
+                    MenuSelection(selection);
+                }
+
+                catch (Exception)
+                {
+                    Console.Write("Improper selection. Make another selection");
                     Console.Clear();
-                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
-                    Console.WriteLine($"Your current balance is {MyBalance}");
-                    // Clears the console buffer and corresponding console window of display information.
-                    break;
+                }
 
-                case 2:
-                    //Make withdrawl from MyBalance
-
-                    //Withdrawl(amount);
-                    //ask user how much they would like to withdraw
-                    Console.WriteLine("How much would you like to withdraw?");
-                    Console.ReadKey();
-                    // Clears the console buffer and corresponding console window of display information.
-                    Console.Clear();
-                    break;
-
-                case 3:
-                    //Make Deposit into MyBalance
-
-                    //Deposit(MyBalance);
-                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
-                    Console.ReadKey();
-                    // Clears the console buffer and corresponding console window of display information.
-                    Console.Clear();
-                    break;
-
-                case 4:
-                    //Loss of retirement fund. Identity Stolen. BANKRUPCY!
-
-                    Donation();
-                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
-                    Console.ReadLine();
-                    // Clears the console buffer and corresponding console window of display information.
-                    Console.Clear();
-                    break;
-
-                case 5:
-                    //Exit of ATM
-
-                    //Exit();
-                    // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
-                    Console.ReadKey();
-                    // Clears the console buffer and corresponding console window of display information.
-                    Console.Clear();
-                    break;
 
             }
+             void MenuSelection(int selection)
+            {
+
+                //switch is a selection statement that chooses a single switch section to execute from a list of candidates based on a pattern match with the match expression.
+                switch (selection)
+                {
+                 case 1:
+                     //Display balance
+                     Console.Clear();
+                     // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                     Console.WriteLine($"Your current balance is {MyBalance}");
+                     // Clears the console buffer and corresponding console window of display information.
+                     break;
+
+                 case 2:
+                     //Make withdrawl from MyBalance
+
+                     int withdrawlAmount;
+                     Console.Clear();
+                     //ask user how much they would like to withdraw
+                     Console.WriteLine("How much would you like to withdraw?");
+                     withdrawlAmount = Convert.ToInt32(Console.ReadLine());
+
+                     // Clears the console buffer and corresponding console window of display information.
+                     if (withdrawlAmount > MyBalance)
+                     {
+                         Console.Clear();
+                         Console.WriteLine(" Insufficient funds. ");
+                     }
+                     else
+                     {
+                         MyBalance = Withdrawl(MyBalance, withdrawlAmount);
+                         Console.Clear();
+                         Console.WriteLine($"{withdrawlAmount} has been withdrawn from your account.");
+
+                     }
+                     break;
+
+                 case 3:
+                        //Make Deposit into MyBalance
+
+                        int addAmount;
+                       
+                     // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                     Console.ReadKey();
+                     // Clears the console buffer and corresponding console window of display information.
+                     Console.Clear();
+                     break;
+
+                 case 4:
+                        //Loss of retirement fund. Identity Stolen. BANKRUPCY!
+
+                       
+                      Console.WriteLine($@"
+                  
+                  
+                            Loss of retirement fund. Identity Stolen.
+                    ´´´´´´´´´´´´´´´´´´´ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´`
+                    ´´´´´´´´´´´´´´´´´¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´
+                    ´´´´´´´´´´´´´¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´´
+                    ´´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´
+                    ´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´`´´´´´´´´´´´¶¶´´´´´´´´´´`
+                    ´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´
+                    ´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+                    ´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+                    ´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
+                    ´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
+                    ´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´
+                    ´´´´´´´´´´´¶¶´¶¶´´´¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´¶¶´¶¶´´´´´´´´´´´
+                    ´´´´´´´´´´´´¶¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶¶¶´´´´´´´´´´´
+                    ´´´´´´´´´´´´´¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶´´´´´´´´´´´´´
+                    ´´´´¶¶¶´´´´´´´¶¶´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´¶¶´´´´´´¶¶¶¶´´´
+                    ´´´¶¶¶¶¶´´´´´¶¶´´´¶¶¶¶¶¶¶´´´¶¶¶´´´¶¶¶¶¶¶¶´´´¶¶´´´´´¶¶¶¶¶¶´´
+                    ´´¶¶´´´¶¶´´´´¶¶´´´´´¶¶¶´´´´¶¶¶¶¶´´´´¶¶¶´´´´´¶¶´´´´¶¶´´´¶¶´´
+                    ´¶¶¶´´´´¶¶¶¶´´¶¶´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´¶¶¶¶´´´´¶¶¶´
+                    ¶¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´¶¶
+                    ¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶¶´´´´´´¶¶¶¶¶¶¶¶
+                    ´´¶¶¶¶´¶¶¶¶¶´´´´´´¶¶¶¶¶´´´´´´´´´´´´´´¶¶¶´¶¶´´´´´¶¶¶¶¶¶´¶¶¶´
+                    ´´´´´´´´´´¶¶¶¶¶¶´´¶¶¶´´¶¶´´´´´´´´´´´¶¶´´¶¶¶´´¶¶¶¶¶¶´´´´´´´´
+                    ´´´´´´´´´´´´´´¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶´´´´´´´´´´´´´´
+                    ´´´´´´´´´´´´´´´´´´¶¶´¶¶´¶´¶´¶´¶´¶´¶´¶´¶´¶¶´´´´´´´´´´´´´´´´´
+                    ´´´´´´´´´´´´´´´´¶¶¶¶´´¶´¶´¶´¶´¶´¶´¶´¶´´´¶¶¶¶¶´´´´´´´´´´´´´´
+                    ´´´´´´´´´´´´¶¶¶¶¶´¶¶´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´´¶¶´¶¶¶¶¶´´´´´´´´´´´´
+                    ´´´´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶´´´´´´´´´´´´´´´´´¶¶´´´´´´¶¶¶¶¶¶¶¶¶´´´´
+                    ´´´¶¶´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´´
+                    ´´´´¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶´´´´
+                    ´´´´´´¶¶´´´¶¶¶´´´´´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´´´´¶¶¶´´´¶¶´´´´´´
+                    ´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´
+                    ´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´ 
+                          ");
+
+
+                     // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                        Console.ReadLine();
+                     // Clears the console buffer and corresponding console window of display information.
+                     Console.Clear();
+                     break;
+
+                 case 5:
+                     //Exit of ATM
+
+                     //Exit();
+                     // Obtains the next character or function key pressed by the user. The pressed key is displayed in the console window.
+                     Console.ReadKey();
+                     // Clears the console buffer and corresponding console window of display information.
+                     Console.Clear();
+                     break;
+
+                }
+
+
+             }
+
         }
-
-        public static void CheckBalance()
-        {
-
-        }
-
-        public static void Donation()
-        {
-            Console.ReadLine();
-
-            Console.WriteLine($@"
-
-
-        Loss of retirement fund. Identity Stolen.
-´´´´´´´´´´´´´´´´´´´ ¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´´´´`
-´´´´´´´´´´´´´´´´´¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´´´´
-´´´´´´´´´´´´´¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´´
-´´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´´
-´´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´`´´´´´´´´´´´¶¶´´´´´´´´´´`
-´´´´´´´´´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´´´´´´´´´
-´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
-´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
-´´´´´´´´´´¶¶´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶´´´´´´´´´´
-´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´¶¶´´´´´´´´´´
-´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´´´´´
-´´´´´´´´´´´¶¶´¶¶´´´¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´¶¶´¶¶´´´´´´´´´´´
-´´´´´´´´´´´´¶¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶¶¶´´´´´´´´´´´
-´´´´´´´´´´´´´¶¶¶´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶´¶¶¶´´´´´´´´´´´´´
-´´´´¶¶¶´´´´´´´¶¶´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´¶¶´´´´´´¶¶¶¶´´´
-´´´¶¶¶¶¶´´´´´¶¶´´´¶¶¶¶¶¶¶´´´¶¶¶´´´¶¶¶¶¶¶¶´´´¶¶´´´´´¶¶¶¶¶¶´´
-´´¶¶´´´¶¶´´´´¶¶´´´´´¶¶¶´´´´¶¶¶¶¶´´´´¶¶¶´´´´´¶¶´´´´¶¶´´´¶¶´´
-´¶¶¶´´´´¶¶¶¶´´¶¶´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´¶¶¶¶´´´´¶¶¶´
-¶¶´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´¶¶
-¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶´´´´¶¶¶¶¶¶¶¶´´´´´´¶¶¶¶¶¶¶¶
-´´¶¶¶¶´¶¶¶¶¶´´´´´´¶¶¶¶¶´´´´´´´´´´´´´´¶¶¶´¶¶´´´´´¶¶¶¶¶¶´¶¶¶´
-´´´´´´´´´´¶¶¶¶¶¶´´¶¶¶´´¶¶´´´´´´´´´´´¶¶´´¶¶¶´´¶¶¶¶¶¶´´´´´´´´
-´´´´´´´´´´´´´´¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶¶¶¶¶¶´¶¶´¶¶¶¶¶¶´´´´´´´´´´´´´´
-´´´´´´´´´´´´´´´´´´¶¶´¶¶´¶´¶´¶´¶´¶´¶´¶´¶´¶¶´´´´´´´´´´´´´´´´´
-´´´´´´´´´´´´´´´´¶¶¶¶´´¶´¶´¶´¶´¶´¶´¶´¶´´´¶¶¶¶¶´´´´´´´´´´´´´´
-´´´´´´´´´´´´¶¶¶¶¶´¶¶´´´¶¶¶¶¶¶¶¶¶¶¶¶¶´´´¶¶´¶¶¶¶¶´´´´´´´´´´´´
-´´´´¶¶¶¶¶¶¶¶¶¶´´´´´¶¶´´´´´´´´´´´´´´´´´¶¶´´´´´´¶¶¶¶¶¶¶¶¶´´´´
-´´´¶¶´´´´´´´´´´´¶¶¶¶¶¶¶´´´´´´´´´´´´´¶¶¶¶¶¶¶¶´´´´´´´´´´¶¶´´´
-´´´´¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶´´´´´¶¶¶¶¶´´´´´¶¶¶´´´´
-´´´´´´¶¶´´´¶¶¶´´´´´´´´´´´¶¶¶¶¶¶¶¶¶´´´´´´´´´´´¶¶¶´´´¶¶´´´´´´
-´´´´´´¶¶´´¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶´´¶¶´´´´´´
-´´´´´´´¶¶¶¶´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´´¶¶¶¶´´´´´´´ 
-                ");
-            Console.ReadLine();
-        }
-
+                  public static int Withdrawl(int MyBalance, int amount)
+                  {
+                      return MyBalance - amount;
+                  }
+                  
+                  //public static void donation()
+                  //{
+                  //    Console.ReadLine();
+                  
+                  //    Console.ReadLine();
     }
 }
+
+
+
+
+
+       
+
+    
+
 
